5096ff016bbb87ec1270d2fee7e20a5ee4c7cd50
Martin Sione <martinsione03@gmail.com>
2021-10-24 13:33:56 -0300

    Initial commit.

---
+++ colors/darkplus.lua
@@ -0,0 +1,2 @@
+package.loaded['darkplus'] = nil
+require 'darkplus'
---
+++ lua/darkplus/init.lua
@@ -0,0 +1,7 @@
+local util = require 'darkplus.util'
+local theme = require 'darkplus.theme'
+
+vim.o.background = 'dark'
+vim.g.colors_name = 'darkplus'
+
+util.load(theme)
